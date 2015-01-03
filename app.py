84f5cfc14e1876d235670369a9b240987d7136b9
Avinash Thummala <avinash.t.iiit@gmail.com>
2015-01-04 05:13:54 +0530

    Added port handling

--- app.py
+++ app.py
@@ -238,4 +238,6 @@ def getXML():
     </memory>''' % (nCols, nCols*nRows/2, app.fileList[int(session['bgIndex'])][11:], app.fileList[int(session['cFIndex'])][11:], getCardTags())
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	
+	port = int(os.environ.get('PORT', 5000)) 
+	app.run(host='0.0.0.0', port=port)	
